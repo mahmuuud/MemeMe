284371fdf676c8e23837be0cf9ea198539abfb41
mahmoud muhammed <34382489+mahmuuud@users.noreply.github.com>
2018-12-09 17:00:29 +0100

    ver 1.0 completed
    picking images, generating memes and sharing them working properly

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var topOutline: UITextField!
     @IBOutlet weak var bottomOutline: UITextField!
@@ -17,6 +18,13 @@ class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControll
     @IBOutlet weak var albumBtn: UIBarButtonItem!
     @IBOutlet weak var actionBtn: UIBarButtonItem!
     @IBOutlet weak var cancelBtn: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    struct Meme{
+        let topText:String
+        let bottomText:String
+        let originalImage:UIImage
+        let memeImage:UIImage
+    }
     let memeTextAttributes:[NSAttributedString.Key:Any]=[
         NSAttributedString.Key(rawValue: NSAttributedString.Key.strokeColor.rawValue):UIColor.black,
         NSAttributedString.Key(rawValue: NSAttributedString.Key.foregroundColor.rawValue):UIColor.white,
@@ -35,6 +43,16 @@ class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControll
         bottomOutline.textAlignment = .center
         actionBtn.isEnabled = !(imageView.image==nil)
         cameraBtn.isEnabled=UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unsubscribeToKeyboardNotifications()
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -70,11 +88,69 @@ class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControll
             let alert=UIAlertController(title: "ERROR loading Image", message: "Something wrong happened ,please try again", preferredStyle: .alert)
             let okAction=UIAlertAction(title: "OK", style: .default, handler: nil)
             alert.addAction(okAction)
+             present(alert,animated: true,completion: nil)
         }
         dismiss(animated: true, completion: nil)
     }
+    //MARK: keyboard related functions
+    func subscribeToKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    func unsubscribeToKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func keyboardWillShow(_ notification:Notification){
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    func getKeyboardHeight(_ notification:Notification)->CGFloat{
+        let userInfo=notification.userInfo
+        let keyboardSize=userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
     
+    @objc func keyboardWillHide(){
+        view.frame.origin.y=0
+    }
+    //MARK: generate the meme image
+    func generateMemeImage()->UIImage?{
+        navBar.isHidden=true
+        toolBar.isHidden=true
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        if let memedImage=UIGraphicsGetImageFromCurrentImageContext(){
+            navBar.isHidden=false
+            toolBar.isHidden=false
+            return memedImage
+        }
+        else{
+            navBar.isHidden=false
+            toolBar.isHidden=false
+            return nil
+        }
+    }
+    //save function is not used yet
+//    func save(){
+//        let meme=Meme(topText: topOutline.text!, bottomText: bottomOutline.text!, originalImage: imageView.image!, memeImage: generateMemeImage()!)
+//    }
 
-
+    @IBAction func share(_ sender: Any) {
+        if let memedImage=generateMemeImage(){
+            let activityVC=UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+              present(activityVC,animated: true,completion: nil)
+        }
+        else{
+            let alert=UIAlertController(title: "Error generating meme image", message: "please try again", preferredStyle: .alert)
+            let okAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert,animated: true,completion: nil)
+        }
+      
+    }
+    
+    
+    
+    
 }
 
